56428b16f50edf25390e2108bb24d101ffb5fb3a
EmbitiousDev <embitiousdev@gmail.com>
2021-11-18 16:45:40 +0100

    refactor(sort menu): shorten popularity and date sort

--- assets/javascript/modules/sorting/dateSort.js
+++ assets/javascript/modules/sorting/dateSort.js
@@ -2,11 +2,7 @@ function dateSort(TILES, DATES) {
     const NUMBERS_FROM_DATES = DATES.map(item => item.replace(/\-/g,''));
     
     for (let i = 0; i < TILES.length; i += 1) {
-        const MAX = Math.max(...NUMBERS_FROM_DATES);
-
-        if (MAX === -1) { return; }
-
-        const index = NUMBERS_FROM_DATES.indexOf(`${MAX}`);
+        const index = NUMBERS_FROM_DATES.findIndex(max => max == Math.max(...NUMBERS_FROM_DATES));
         NUMBERS_FROM_DATES.splice(index, 1, "-1");
 
         TILES[index].style.order = i;
--- assets/javascript/modules/sorting/popularitySort.js
+++ assets/javascript/modules/sorting/popularitySort.js
@@ -1,12 +1,8 @@
 function popularitySort(TILES, LIKES) {
     for(let i = 0; i < TILES.length; i += 1) {
-        const max = Math.max(...LIKES);
-
-        if (max === -1) { return; }
-
-        const index = LIKES.indexOf(`${max}`);
+        const index = LIKES.findIndex(max => max == Math.max(...LIKES));
         LIKES.splice(index, 1, "-1");
-
+        
         TILES[index].style.order = i;
     }
 }
