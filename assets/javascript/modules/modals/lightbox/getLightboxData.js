fdf9fead543d47669de2f4c92acc11d4fa7b7773
EmbitiousDev <embitiousdev@gmail.com>
2021-11-21 13:31:21 +0100

    feat(video): add controls

--- assets/javascript/modules/modals/lightbox/getLightboxData.js
+++ assets/javascript/modules/modals/lightbox/getLightboxData.js
@@ -4,6 +4,11 @@ function getLightboxData(MODAL_BASICS) {
     MODAL_BASICS.lightbox.openButtons.forEach(element => {
         const MEDIA = element.cloneNode(false);
         MEDIA.className = 'lightbox__media';
+
+        if (MEDIA.tagName === 'VIDEO') {
+            MEDIA.setAttribute('controls', '');
+        }
+
         const TITLE = element.parentElement.lastElementChild.querySelector('.portfolio__element-title').innerText;
         LIGHTBOX_DATA.push({
             'media': MEDIA,
--- assets/javascript/modules/modals/lightbox/lightboxNavigation.js
+++ assets/javascript/modules/modals/lightbox/lightboxNavigation.js
@@ -72,6 +72,19 @@ function lightboxNavigation(LIGHTBOX_DATA, START_ID, LIGHTBOX_MEDIA, LIGHTBOX_TI
             LIGHTBOX_TITLE.innerText = LIGHTBOX_DATA[i].title;
         }
     })
+
+    window.addEventListener('keydown', event => {
+        if (event.key === ' ') {
+            const mediaElement = document.querySelector('.lightbox__media');
+            if (mediaElement.tagName === 'VIDEO') {
+                if (mediaElement.paused) {
+                    mediaElement.play();
+                } else {
+                    mediaElement.pause();
+                }
+            }
+        }
+    });
 }
 
 export default lightboxNavigation;
