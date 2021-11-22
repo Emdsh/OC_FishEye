ef5b9f1ef1fa7307c04151b36c5e01d680988087
EmbitiousDev <embitiousdev@gmail.com>
2021-11-22 16:58:57 +0100

    fix(lightbox): media plays on space keey press consistently

--- assets/javascript/modules/modals/lightbox/lightboxMediaPlay.js
+++ assets/javascript/modules/modals/lightbox/lightboxMediaPlay.js
@@ -1,18 +1,18 @@
-function lightboxMediaPlay(lightboxOpen) {
-    function playMedia(event) {
-        if (event.key === ' ') {
-            const mediaElement = document.querySelector('.lightbox__media');
-            if (mediaElement.tagName === 'VIDEO') {
-                if (mediaElement.paused) {
-                    return mediaElement.play();
-                }
-                if (mediaElement.play) {
-                    return mediaElement.pause();
-                }
+function playMedia(event) {
+    if (event.key === ' ') {
+        const mediaElement = document.querySelector('.lightbox__media');
+        if (mediaElement.tagName === 'VIDEO') {
+            if (mediaElement.paused) {
+                return mediaElement.play();
+            }
+            if (mediaElement.play) {
+                return mediaElement.pause();
             }
         }
     }
+}
 
+function lightboxMediaPlay(lightboxOpen) {
     if (lightboxOpen === true) {
         document.body.addEventListener('keydown', playMedia);
     }
