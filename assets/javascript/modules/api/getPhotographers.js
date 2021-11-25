97ebcbc13aa96c89ec740fad35a6bf1fbfe43553
EmbitiousDev <embitiousdev@gmail.com>
2021-11-25 11:57:21 +0100

    fix(GitHub Pages): hack paths for them to work on GitHub

--- assets/javascript/modules/api/getPhotographers.js
+++ assets/javascript/modules/api/getPhotographers.js
@@ -1,7 +1,7 @@
 async function getPhotographers(PATH) {
     if (PATH === '/BaptisteLizot_5_07902021_pre10112021/') {
         try {
-            const response = await fetch('./assets/api/api.json', {
+            const response = await fetch('../assets/api/api.json', {
                 headers: {
                     'Content-Type': 'application/json',
                     'Accept': 'application/json'
@@ -16,7 +16,7 @@ async function getPhotographers(PATH) {
 
     if (PATH === '/BaptisteLizot_5_07902021_pre10112021/photographer/') {
         try {
-            const response = await fetch('./assets/api/api.json', {
+            const response = await fetch('../assets/api/api.json', {
                 headers: {
                     'Content-Type': 'application/json',
                     'Accept': 'application/json'
